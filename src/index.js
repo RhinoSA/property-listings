9873991027725bd297bf3f6be4dd819871112ba3
Andrew van Ginkel <andrewvg@imaginet.co.za>
2019-08-11 16:12:25 +0200

    Latest build

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import * as serviceWorker from "./serviceWorker";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "bootstrap/dist/js/jquery.min.js";
 
 import { RoomProvider } from "./contextrooms";
 
