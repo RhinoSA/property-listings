65dec2a26f847143ac4427b2fa92b01a1225e89c
Andrew van Ginkel <andrewvg@imaginet.co.za>
2019-10-31 01:16:20 +0200

    new build

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -3,8 +3,8 @@ import Navbar from "../modules/navbar/navbar";
 import Menu from "../../sections/menu";
 import FeaturedRooms from "./featuredrooms";
 import Photo from "../../images/lion.jpg";
-//import { TimelineMax, TweenMax } from "gsap/all";
-import * as TweenMax from "gsap/umd/TweenMax";
+import { TimelineMax, TweenMax } from "gsap/all";
+//import * as TweenMax from "gsap/umd/TweenMax";
 
 /* ########## BLIP ########## */
 import "../../sections/extras/svg/blip/blip.css";
